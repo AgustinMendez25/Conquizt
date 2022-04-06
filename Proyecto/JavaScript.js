d5b8c573b9c922628db4d62a43340743c84c46d2
Mate1209 <74111936+Mate1209@users.noreply.github.com>
2022-04-06 16:14:43 -0300

    Add files via upload

---
+++ Proyecto/JavaScript.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+<title> Prueba de JavaScript </title>
+
+</head>
+<body>
+
+<script>
+document.getElementsByTagName("jugadores");
+document.getElementsByTagName("partidas");
+document.getElementsByTagName("preguntas");
+document.getElementsByTagName("categoria");
+</script>
+
+</body>
+</html>
