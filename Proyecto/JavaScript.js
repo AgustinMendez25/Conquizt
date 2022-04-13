c94f203b01999a7ed8a2b3e7cb796ef131f481bb
Mate1209 <74111936+Mate1209@users.noreply.github.com>
2022-04-13 17:09:43 -0300

    JavaScript BDD

--- Proyecto/JavaScript.js
+++ Proyecto/JavaScript.js
@@ -1,17 +1,40 @@
 <html>
+
 <head>
 
-<title> Prueba de JavaScript </title>
+<meta charset="UTF-8"></meta>
+
+<title> JavaScript BDD </title>
 
 </head>
+
 <body>
 
 <script>
-document.getElementsByTagName("jugadores");
-document.getElementsByTagName("partidas");
-document.getElementsByTagName("preguntas");
-document.getElementsByTagName("categoria");
+
+const x = document.getElementById("jugadores");
+const y = x.getElementsByTagName("info");
+const y = x.getElementsByTagName("idjugador");
+const y = x.getElementsByTagName("ptsacumulados");
+
+const x = document.getElementById("partidas");
+const y = x.getElementsByTagName("territorio");
+const y = x.getElementsByTagName("datosjugador");
+const y = x.getElementsByTagName("cartaseleccionada");
+
+const x = document.getElementById("preguntas");
+const y = x.getElementsByTagName("idpregunta");
+const y = x.getElementsByTagName("pregunta");
+const y = x.getElementsByTagName("dificultad");
+const y = x.getElementsByTagName("categoria");
+const y = x.getElementsByTagName("interfaz");
+
+const x = document.getElementById("categoria");
+const y = x.getElementsByTagName("idcategoria");
+const y = x.getElementsByTagName("tipocategoria");
+
 </script>
 
 </body>
+
 </html>
