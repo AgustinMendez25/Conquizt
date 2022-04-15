88c15bbc712f380e55aa60163ea7d71a269352b5
AgustinMendez25 <74325903+AgustinMendez25@users.noreply.github.com>
2022-04-15 15:20:27 -0300

    Agregados de carpeta de campo
    Se completaron imagenes de carpeta de campo de dias anteriores (desde el 5-04 hasta el 13-04).
    Además, se reorganizaron algunas carpetas y archivos fuera de su lugar.

---
+++ Proyecto/js/JavaScript.js
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+
+<meta charset="UTF-8"></meta>
+
+<title> JavaScript BDD </title>
+
+</head>
+
+<body>
+
+<script>
+
+const x = document.getElementById("jugadores");
+const y = x.getElementsByTagName("info");
+const y = x.getElementsByTagName("idjugador");
+const y = x.getElementsByTagName("ptsacumulados");
+
+const x = document.getElementById("partidas");
+const y = x.getElementsByTagName("territorio");
+const y = x.getElementsByTagName("datosjugador");
+const y = x.getElementsByTagName("cartaseleccionada");
+
+const x = document.getElementById("preguntas");
+const y = x.getElementsByTagName("idpregunta");
+const y = x.getElementsByTagName("pregunta");
+const y = x.getElementsByTagName("dificultad");
+const y = x.getElementsByTagName("categoria");
+const y = x.getElementsByTagName("interfaz");
+
+const x = document.getElementById("categoria");
+const y = x.getElementsByTagName("idcategoria");
+const y = x.getElementsByTagName("tipocategoria");
+
+</script>
+
+</body>
+
+</html>
