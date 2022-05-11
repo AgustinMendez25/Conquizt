1951adb062f93ef46c51a831c7c0cf457691e407
AgustinMendez25 <74325903+AgustinMendez25@users.noreply.github.com>
2022-05-11 17:40:32 -0300

    Provisorio Avances 11-05
    Avances en rama secundaria de modales y preguntas (funcionalidad)

--- Proyecto/js/modal.js
+++ Proyecto/js/modal.js
@@ -1,9 +1,13 @@
 "use strict";
 
+/*Modales Ajustes*/
+
 const btnAjustes = document.getElementById("btnAjustes");
 
 const modalAjustes = document.getElementById("modalAjustes");
 const modalAjustesCaja = document.getElementById("modalAjustesCaja");
+
+const cerrarAjustes = document.getElementById("cerrarAjustes");
 
 btnAjustes.addEventListener("click", ()=>{
     modalAjustes.style.visibility = "visible";
@@ -15,4 +19,56 @@ cerrarAjustes.addEventListener("click", ()=>{
     modalAjustes.style.visibility = "hidden";
     modalAjustes.style.opacity = "0";
     modalAjustesCaja.style.transform = "translateY(-30%)";
+})
+
+/*Modales Atacar Defender*/
+
+const atacarBtn = document.getElementById("atacarBtn");
+const defenderBtn = document.getElementById("defenderBtn");
+
+const modalAtacar = document.getElementById("modalAtacar");
+const modalDefender = document.getElementById("modalDefender");
+
+const cerrarModalDefender = document.getElementById("cerrarModalDefender");
+const cerrarModalAtacar = document.getElementById("cerrarModalAtacar");
+
+defenderBtn.addEventListener("click", ()=>{
+    modalAtacar.style.visibility = "hidden";
+    modalAtacar.style.opacity = "0";
+    modalDefender.style.visibility = "visible";
+    modalDefender.style.opacity = "1";
+})
+
+cerrarModalDefender.addEventListener("click", ()=>{
+    modalDefender.style.visibility = "hidden";
+    modalDefender.style.opacity = "0";
+})
+
+atacarBtn.addEventListener("click", ()=>{
+    modalAtacar.style.visibility = "visible";
+    modalAtacar.style.opacity = "1";
+    modalDefender.style.visibility = "hidden";
+    modalDefender.style.opacity = "0";
+})
+
+cerrarModalAtacar.addEventListener("click", ()=>{
+    modalAtacar.style.visibility = "hidden";
+    modalAtacar.style.opacity = "0";
+})
+
+/*Modal Preguntas*/
+
+const btnConfirmarDefensa = document.getElementById("btnConfirmarDefensa");
+const btnConfirmarAtaque = document.getElementById("btnConfirmarAtaque");
+
+const modalPreguntas = document.getElementById("modalPreguntas");
+
+btnConfirmarDefensa.addEventListener("click", ()=>{
+    modalPreguntas.style.opacity = "1";
+    modalPreguntas.style.visibility = "visible";
+})
+
+btnConfirmarAtaque.addEventListener("click", ()=>{
+    modalPreguntas.style.opacity = "1";
+    modalPreguntas.style.visibility = "visible";
 })
--- Proyecto/js/partida.js
+++ Proyecto/js/partida.js
@@ -1,16 +1,79 @@
 "use strict";
 
+/*Turno y ronda*/
+
 const siguienteTurno = document.getElementById("siguienteTurno");
 const nombreJugadorTurno = document.getElementById("nombreJugadorTurno");
+const ronda = document.getElementById("ronda");
 
 const cantJugadores = 4;
 let turnoJugador = 1;
+let numRonda = 1;
 
-siguienteTurno.addEventListener("click", ()=>{
+/*Preguntas*/
+
+function pasarTurno(){
     if(turnoJugador >= cantJugadores){
         turnoJugador = 1;
+        numRonda++;
     }else{
         turnoJugador++;
     }
     nombreJugadorTurno.innerHTML = "Jugador " + turnoJugador;
+    ronda.innerHTML = numRonda;
+}
+
+function darTiempo() {
+    const modalPreguntas = document.getElementById("modalPreguntas");
+
+    const Tiempo = setTimeout(()=>{
+        modalPreguntas.style.visibility = "hidden";
+        modalPreguntas.style.opacity = "0";
+        pasarTurno();
+    }, 3000);
+}
+
+const btnRespuesta1 = document.getElementById("respuesta1");
+const btnRespuesta2 = document.getElementById("respuesta2");
+const btnRespuesta3 = document.getElementById("respuesta3");
+const btnRespuesta4 = document.getElementById("respuesta4");
+
+const respCorrecta = btnRespuesta3.getAttribute("id");
+
+btnRespuesta1.addEventListener("click", ()=>{
+    if(btnRespuesta1.getAttribute("id") == respCorrecta){
+        btnRespuesta1.style.backgroundColor = "green";
+    }else{
+        btnRespuesta1.style.backgroundColor = "red";
+    }
+    darTiempo();
 })
+
+btnRespuesta2.addEventListener("click", ()=>{
+    if(btnRespuesta2.getAttribute("id") == respCorrecta){
+        btnRespuesta2.style.backgroundColor = "green";
+    }else{
+        btnRespuesta2.style.backgroundColor = "red";
+    }
+    darTiempo();
+})
+
+btnRespuesta3.addEventListener("click", ()=>{
+    if(btnRespuesta3.getAttribute("id") == respCorrecta){
+        btnRespuesta3.style.backgroundColor = "green";
+    }else{
+        btnRespuesta3.style.backgroundColor = "red";
+    }
+    darTiempo();
+})
+
+btnRespuesta4.addEventListener("click", ()=>{
+    if(btnRespuesta4.getAttribute("id") == respCorrecta){
+        btnRespuesta4.style.backgroundColor = "green";
+    }else{
+        btnRespuesta4.style.backgroundColor = "red";
+    }
+    darTiempo();
+})
+
+
