5eb15c874729c2d4620ec590d362fbd90ad7e4f0
BlaitzMax <74390390+BlaitzMax@users.noreply.github.com>
2022-06-14 14:03:05 -0300

    Base de datos SQL
    Base de datos 14/06/2022

---
+++ Proyecto/Base de Datos/conquizt.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS conquizt;
+
+CREATE DATABASE conquizt;
+
+USE conquizt;
+
+CREATE TABLE civilizaciones(
+
+    idCivilizacion INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tipoCivilizacion VARCHAR(20) NOT NULL
+
+)
+
+CREATE TABLE jugadores(
+
+    idJugador INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(12) NOT NULL,
+    ptsAcumulados INTEGER
+
+)
+
+CREATE TABLE categoria(
+
+    idCategoria INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tipoCategoria VARCHAR(20) NOT NULL
+
+)
